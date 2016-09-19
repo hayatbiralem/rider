0c956cab3b0df03078df71f086c042ddd7876751
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2016-09-19 11:51:58 +0300

    CustomEvent bug fixed

--- dist/core.js
+++ dist/core.js
@@ -457,9 +457,6 @@
         Rider.on(obj, type, func);
     };
 
-    // init RiderResize event
-    Rider.throttle("resize", "RiderResize");
-
     // plugins
     Rider.plugins = [];
     Rider.addPlugin = function (key, Plugin) {
@@ -468,61 +465,22 @@
         Rider.plugins.push(Rider[key]);
     };
 
-    // returns version of IE or false, if browser is not Internet Explorer
-    Rider.detectIE = function () {
-        var ua = window.navigator.userAgent;
-
-        // Test values; Uncomment to check result …
-
-        // IE 10
-        // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-
-        // IE 11
-        // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-
-        // Edge 12 (Spartan)
-        // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-
-        // Edge 13
-        // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
-        var msie = ua.indexOf('MSIE ');
-        if (msie > 0) {
-            // IE 10 or older => return version number
-            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-        }
-
-        var trident = ua.indexOf('Trident/');
-        if (trident > 0) {
-            // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
-            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-        }
-
-        var edge = ua.indexOf('Edge/');
-        if (edge > 0) {
-            // Edge (IE 12+) => return version number
-            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-        }
-
-        // other browser
-        return false;
+    // Pollyfill for CustomEvent() Constructor - thanks to Internet Explorer
+    // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+    var CustomEvent = function (event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
     };
 
-    if(Rider.detectIE()){
-        // Pollyfill for CustomEvent() Constructor - thanks to Internet Explorer
-        // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
-        var CustomEvent = function (event, params) {
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-            var evt = document.createEvent('CustomEvent');
-            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-            return evt;
-        };
+    CustomEvent.prototype = global.CustomEvent ? global.CustomEvent.prototype : {};
+    global.CustomEvent = CustomEvent;
 
-        CustomEvent.prototype = global.CustomEvent ? global.CustomEvent.prototype : {};
-        global.CustomEvent = CustomEvent;
-    }
+    // init RiderResize event
+    Rider.throttle("resize", "RiderResize");
 
+    // done
     global.Rider = Rider;
 
 })(this);
